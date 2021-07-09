b76e487824750df398f1b10b2a8e83a2dbeb43b0
nahumar <81519500+nahumar@users.noreply.github.com>
2021-07-08 22:07:19 -0300

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,12 @@
 
 
 function saludar() {
-	alert("Hola, buenas");
+	var saludo = document.getElementById("mi_boton");
+	//saludo.disabled = true;
+	saludo.hidden = true;
+	
+	var resultado_saludo = document.getElementById("resultado_saludo");
+	resultado_saludo.innerText = "Ya Salude!"
+	resultado_saludo.style.background = '#00fff0';
+	resultado_saludo.style.width = '75px'
 }
